186330f70ac07f89d1238f1df5b4cf77766b7f8b
Simon Whitaker <sw@netcetera.org>
2013-12-26 22:06:24 +0000

    Add .gitignore, remove boilerplate tests

---
+++ WWDC Video Search/SWSessionsDataSource.h
@@ -0,0 +1,13 @@
+//
+//  SWSessionsDataSource.h
+//  WWDC Video Search
+//
+//  Created by Simon Whitaker on 26/12/2013.
+//  Copyright (c) 2013 Netcetera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWSessionsDataSource : NSObject
+
+@end
---
+++ WWDC Video Search/SWSessionsDataSource.m
@@ -0,0 +1,13 @@
+//
+//  SWSessionsDataSource.m
+//  WWDC Video Search
+//
+//  Created by Simon Whitaker on 26/12/2013.
+//  Copyright (c) 2013 Netcetera. All rights reserved.
+//
+
+#import "SWSessionsDataSource.h"
+
+@implementation SWSessionsDataSource
+
+@end
