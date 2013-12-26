80f1b0d4f2829c52a3ef3267f707a7f66ef31683
Simon Whitaker <sw@netcetera.org>
2013-12-26 22:51:37 +0000

    Change organisation name

--- WWDC Video Search/SWAppDelegate.h
+++ WWDC Video Search/SWAppDelegate.h
@@ -3,7 +3,7 @@
 //  WWDC Video Search
 //
 //  Created by Simon Whitaker on 26/12/2013.
-//  Copyright (c) 2013 Netcetera. All rights reserved.
+//  Copyright (c) 2013 Simon Whitaker. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
