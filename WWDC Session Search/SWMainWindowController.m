be1870fb0a1fda0f5e686bd66a73137b755c8778
Simon Whitaker <sw@netcetera.org>
2013-12-31 21:48:18 +0000

    Add search term highlighting to results

--- WWDC Session Search/SWMainWindowController.m
+++ WWDC Session Search/SWMainWindowController.m
@@ -16,6 +16,23 @@ static NSString const *kResultsSessionNumberKey = @"session_number";
 static NSString const *kResultsTitleKey = @"title";
 static NSString const *kResultsDescriptionKey = @"description";
 static NSString const *kResultsTrackKey = @"track";
+static NSString const *kResultsOffsetsKey = @"offsets";
+
+NSUInteger characterOffsetForByteOffsetInUTF8String(NSUInteger byteOffset, const char *string) {
+    NSUInteger result = 0;
+    for (NSUInteger i = 0; i < byteOffset; i++) {
+        char c = string[i];
+        if (c & 0x80) {
+            // We're at the first byte of a UTF-8 character. Move to the last byte.
+            while ((string[i+1] & 0x80) && i < byteOffset) {
+                i++;
+            }
+        }
+        result++;
+    }
+    return result;
+}
+
 
 @interface SWMainWindowController()
 @property (nonatomic) NSString *databaseFilePath;
@@ -83,7 +100,7 @@ static NSString const *kResultsTrackKey = @"track";
 - (sqlite3_stmt *)searchResultsQuery {
     if (!_searchResultsQuery) {
         // session is a plain old table, session_fts is a virtual full text search table. See http://www.sqlite.org/fts3.html
-        NSString *queryString = @"SELECT s.year, s.session_number, s.title, s.description, s.track FROM session s, session_fts WHERE s.rowid = session_fts.docid AND session_fts MATCH ?";
+        NSString *queryString = @"SELECT s.year, s.session_number, s.title, s.description, s.track, offsets(session_fts) FROM session s, session_fts WHERE s.rowid = session_fts.docid AND session_fts MATCH ?";
         sqlite3_stmt *stmt;
         int result = sqlite3_prepare_v2(self.db, [queryString UTF8String], -1, &stmt, NULL);
         if (result == SQLITE_OK) {
@@ -151,7 +168,15 @@ static NSString const *kResultsTrackKey = @"track";
     NSString *identifier = tableColumn.identifier;
     if ([identifier isEqualToString:@"MainCell"]) {
         SWSessionsTableCellView *cellView = [tableView makeViewWithIdentifier:identifier owner:self];
-        cellView.titleField.stringValue = cellData[kResultsTitleKey];
+        
+        // Get the title, possibly including highlighted search terms
+        NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:cellData[kResultsTitleKey]];
+        
+        for (NSValue *highlightRangeObj in cellData[kResultsOffsetsKey][kResultsTitleKey]) {
+            [title addAttribute:NSBackgroundColorAttributeName value:[[NSColor yellowColor] colorWithAlphaComponent:0.4] range:[highlightRangeObj rangeValue]];
+        }
+        
+        cellView.titleField.attributedStringValue = title;
         cellView.sessionIdField.stringValue = [cellData[kResultsSessionNumberKey] description];
         cellView.trackField.stringValue = [NSString stringWithFormat:@"%@ (%@)", cellData[kResultsTrackKey], cellData[kResultsYearKey]];
 
@@ -192,14 +217,54 @@ static NSString const *kResultsTrackKey = @"track";
             const char *title = (const char*)sqlite3_column_text(query, 2);
             const char *description = (const char*)sqlite3_column_text(query, 3);
             const char *track = (const char*)sqlite3_column_text(query, 4);
+
+            NSMutableDictionary *result = [NSMutableDictionary dictionary];
+            result[kResultsSessionNumberKey] = @(session_number);
+            result[kResultsTitleKey] = [NSString stringWithCString:title encoding:NSUTF8StringEncoding];
+            result[kResultsDescriptionKey] = [NSString stringWithCString:description encoding:NSUTF8StringEncoding];
+            result[kResultsTrackKey] = [NSString stringWithCString:track encoding:NSUTF8StringEncoding];
+            result[kResultsYearKey] = @(year);
             
-            [mutableResults addObject:@{
-                kResultsSessionNumberKey: @(session_number),
-                kResultsTitleKey: [NSString stringWithCString:title encoding:NSUTF8StringEncoding],
-                kResultsDescriptionKey: [NSString stringWithCString:description encoding:NSUTF8StringEncoding],
-                kResultsTrackKey: [NSString stringWithCString:track encoding:NSUTF8StringEncoding],
-                kResultsYearKey: @(year),
-            }];
+            if (isSearching) {
+                NSMutableDictionary *offsets = [NSMutableDictionary dictionary];
+                static NSArray *resultColumns;
+                if (!resultColumns) {
+                    resultColumns = @[kResultsSessionNumberKey, kResultsTitleKey, kResultsDescriptionKey];
+                
+                }
+                NSString *offsetsString = [NSString stringWithCString:(const char*)sqlite3_column_text(query, 5) encoding:NSUTF8StringEncoding];
+                NSArray *offsetComponents = [offsetsString componentsSeparatedByString:@" "];
+                for (NSUInteger i = 0; i < [offsetComponents count]; i += 4) {
+                    NSInteger columnIndex = [offsetComponents[i] integerValue];
+                    NSInteger byteOffset = [offsetComponents[i+2] integerValue];
+                    NSInteger byteLength = [offsetComponents[i+3] integerValue];
+                    
+                    NSString *key = resultColumns[columnIndex];
+                    if (!offsets[key]) {
+                        offsets[key] = [NSMutableArray array];
+                    }
+                    
+                    NSUInteger charOffset = (NSUInteger)byteOffset;
+                    NSUInteger endCharOffset = (NSUInteger)(byteOffset + byteLength);
+                    NSUInteger charLength = (NSUInteger)byteLength;
+                    
+                    // Convert byte offset to character offset for title and description fields (which might contain non-ASCII UTF-8 characters)
+                    if (key == kResultsTitleKey) {
+                        charOffset = characterOffsetForByteOffsetInUTF8String(charOffset, title);
+                        endCharOffset = characterOffsetForByteOffsetInUTF8String(endCharOffset, title);
+                        charLength = endCharOffset - charOffset;
+                    } else if (key == kResultsDescriptionKey) {
+                        charOffset = characterOffsetForByteOffsetInUTF8String(charOffset, description);
+                        endCharOffset = characterOffsetForByteOffsetInUTF8String(endCharOffset, description);
+                        charLength = endCharOffset - charOffset;
+                    }
+                    NSRange range = NSMakeRange(charOffset, charLength);
+                    [(NSMutableArray*)offsets[key] addObject:[NSValue valueWithRange:range]];
+                }
+                result[kResultsOffsetsKey] = offsets;
+            }
+            
+            [mutableResults addObject:result];
         }
     }
     
